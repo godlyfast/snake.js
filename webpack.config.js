b9d6d955778568a6e59afbe242b15159bc7e2c11
Tim Smyr <godlyfast@gmail.com>
2020-10-07 14:46:41 +0300

    wp conf

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: "bundle.js",
+    filename: "snake.bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
 };
