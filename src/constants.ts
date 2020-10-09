a0ad9a3b2a0cfcf83964a853b11a71360fe66bb5
Tim Smyr <godlyfast@gmail.com>
2020-10-09 13:18:39 +0300

    constants edit

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,11 @@
-export const CANVAS_WIDTH = 600;
-export const CANVAS_HEIGHT = 400;
-export const BLOCK_SIZE = 5;
-export const GAME_BLOCK_SIZE = 5;
+export const CANVAS_WIDTH = 1200;
+export const CANVAS_HEIGHT = 1000;
+export const BLOCK_SIZE = 10;
+export const GAME_BLOCK_SIZE = 10;
 export const MIN_CLOCK = 30;
 export const MAX_CLOCK = 50;
 export const GAME_STATUS_OVER = "GAME_OVER";
 export const GAME_STATUS_INIT = "GAME_INIT";
 export const GAME_STATUS_IN_PROGRESS = "GAME_IN_PROGRESS";
 export const MAX_NAME_LEN = 4;
-export const TEXT_SIZE = 25;
+export const TEXT_SIZE = 50;
